35736b188a1d7ebbae46de536ac068cf228a938e
anseonghyeon <anseonghyeon@gmail.com>
2020-04-21 00:22:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ while True:
     if type(location) != type(None):
         pyautogui.click(location)
         break
+while True:
+    location = pyautogui.locateCenterOnScreen("ok_btn.png")
+    if type(location) != type(None):
+        pyautogui.click(location)
+        pyautogui.click(location[0],location[1]-100)
+        break
+while True:
+    location = pyautogui.locateCenterOnScreen("search_btn.png")
+    if type(location) != type(None):
+        pyautogui.click(location)
+        break
+while True:
+    location = pyautogui.locateCenterOnScreen("surak_btn.png")
+    if type(location) != type(None):
+        pyautogui.click(location)
+        break
 
 # print(location)
 # while True:
