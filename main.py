847500fb8ac241ac83ebda96ca46dd993f0ef27f
anseonghyeon <anseonghyeon@gmail.com>
2020-04-21 20:07:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pyautogui
-# import cv2
 
 contents = "wr"
+location = None
+
 
 while True:
+    
+    # elise
+    while True:
+        location = pyautogui.locateCenterOnScreen("images/elise_icon.png")
+        if location is not None:
+            break
+    pyautogui.click(location)
+    
     # chat
     while True:
         location = pyautogui.locateCenterOnScreen("images/chat_window.png")
-        if type(location) != type(None):
+        if location is not None:
             break
     pyautogui.click(location)
     for i in range(10):
         pyautogui.write(contents)
         pyautogui.press("enter")
 
-    # elise
-    while True:
-        location = pyautogui.locateCenterOnScreen("images/elise_icon.png")
-        if type(location) != type(None):
-            break
-    pyautogui.click(location)
-
     # ready
     while True:
         location = pyautogui.locateCenterOnScreen("images/ready_button.png")
-        if type(location) != type(None):
+        if location is not None:
             break
     pyautogui.click(location)
