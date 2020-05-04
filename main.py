c368c9fe50f8118007cc917a099966cf8e34cda2
anseonghyeon <anseonghyeon@gmail.com>
2020-05-04 23:16:57 +0900

    Update main.py README.md

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import pyautogui
 import time
-import cv2
+# import cv2
 
-location = None
 position = input("Please enter the position you want: ")
-champion = input("Please enter the champion you want: ")
+if len(position) > 10:
+    print("Please enter position within 10 characters")
+    exit(1)
 
-if len(position) > 10 and len(champion) >10:
-    print("Please enter 10 word")
+champion = input("Please enter the champion you want: ")
+if len(champion) > 10:
+    print("Please enter champion within 10 characters")
     exit(1)
 
 while True:
     # champion
     while True:
-        location = pyautogui.locateCenterOnScreen("images/champion/"+champion+".png",confidence=0.9)
-        time.sleep(0.1)
-        if location is not None:
-            break
+        try:
+            location = pyautogui.locateCenterOnScreen("images/champion/"+champion+".png",confidence=0.9)
+            if location is not None:
+                break
+            time.sleep(0.01)    
+        except OSError:
+            print("없는 챔피언입니다.")
+            exit(1)
     pyautogui.click(location)
-
     # position
     while True:
-        location = pyautogui.locateCenterOnScreen("images/chat_window.png")
+        location = pyautogui.locateCenterOnScreen("images/chat_window.png",confidence=0.8)
         if location is not None:
             break
     pyautogui.click(location)
     for i in range(10):
         pyautogui.write(position)
         pyautogui.press("enter")
-        
     # ready
     while True:
-        location = pyautogui.locateCenterOnScreen("images/ready_button.png")
+        location = pyautogui.locateCenterOnScreen("images/ready_button.png",confidence=0.8)
         if location is not None:
             break
     pyautogui.click(location)
